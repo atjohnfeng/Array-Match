181d3d520f0bc9a2b6aa286033485e61b8179b27
atjohnfeng <atjohnfeng@gmail.com>
2021-09-04 23:44:26 -0700

    Add hand board divider gradient

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -9,7 +9,19 @@ function loadLevel(level) { // #TODO
     let arrEls = level.getArrayEl();
 }
 
-loadLevel(currentLvl);
+// loadLevel(currentLvl);
+
+var gradient = board.createLinearGradient(0, 0, 600, 0);
+gradient.addColorStop("0", "lightpink");
+gradient.addColorStop("0.5", "skyblue");
+gradient.addColorStop("1.0", "royalblue");
+
+board.beginPath();
+board.strokeStyle = gradient;
+board.setLineDash([]);
+board.moveTo(0, canvas.height * .666);
+board.lineTo(canvas.width, canvas.height * .666);
+board.stroke();
 
 // board.fillStyle = "blue";
 // board.fillRect(100, 100, 50, 50);
